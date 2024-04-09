64255f0ecba08649e33f4a94eb55f7999a10b21b
DivyarajsinhRana <divyarajsinh.rana@archesoftronix.com>
2024-04-09 12:56:40 +0530

    navbar and hero section in-progress

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,31 @@
-import { headerLogo } from "../assets/images"
+import { hamburger } from '../assets/icons'
+import { headerLogo } from '../assets/images'
+import { navLinks } from '../constant'
 
 const Nav = () => {
     return (
-        <header>
-            <nav>
+        <header className="padding-x py-8 absolute z-10 w-full">
+            <nav className="flex justify-between items-center max-container">
                 <a href="/">
                     <img src={headerLogo} alt="logo" />
                 </a>
+                <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+                    {navLinks.map((item) => {
+                        return (
+                            <li key={item.label}>
+                                <a
+                                    href={item.href}
+                                    className="font-montserrat leading-normal text-lg text-slate-gray"
+                                >
+                                    {item.label}
+                                </a>
+                            </li>
+                        )
+                    })}
+                </ul>
+                <div className='hidden max-lg:block'>
+                    <img src={hamburger} alt="menu" width={25} height={25} className='cursor-pointer' />
+                </div>
             </nav>
         </header>
     )
--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -1,6 +1,20 @@
+import { arrowRight } from "../assets/icons"
+import Button from "../components/Button"
+
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <section id='home' className="w-full flex flex-col xl:flex-row gap-10 min-h-screen max-container">
+            <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:paddig-x pt-28">
+                <p>Our Summer Collection</p>
+                <h1>
+                    <span>The New Arrical</span>
+                    <br />
+                    <span>Nike</span>shoes
+                </h1>
+                <p>Discover stylish Nike arrivals, quality comfort and innvovation for your active life.</p>
+                <Button label='Shop Now' icon={arrowRight} />
+            </div>
+        </section>
     )
 }
 
